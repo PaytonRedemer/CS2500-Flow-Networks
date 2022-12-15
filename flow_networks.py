d44038a3b96585e49ae4b379e6d63eb0d84373fc
Payton Redemer <paytonredemer@gmail.com>
2022-12-14 19:30:44 -0600

    add formatting

--- flow_networks.py
+++ flow_networks.py
@@ -3,12 +3,14 @@ import csv
 
 network = []
 
-print(sys.argv[1])
-
 with open(sys.argv[1], mode= 'r') as file:
     csvFile = csv.reader(file)
 
     for line in csvFile:
         network.append([line[0],line[1],int(line[2]),0])
 
-print(network)
+
+print(f'Input File: {sys.argv[1]}\n')
+print("Input:")
+for edge in network:
+    print(f'{edge[0]},{edge[1]},{edge[2]}')
