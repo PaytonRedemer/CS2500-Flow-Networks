3e77a2bf076fc1717a842f261781a7e2a99855a9
Payton Redemer <paytonredemer@gmail.com>
2022-12-16 00:25:23 -0600

    basic working max flow

--- flow_networks.py
+++ flow_networks.py
@@ -1,17 +1,14 @@
 import sys
 import csv
 
-
-max_flow = 0
-
 class flow_networks:
 
     def __init__(self, file):
         self.file = file
         self.network = []
-        self.residual_network = []
         self.nodes_index = {}
         self.parse_file()
+        self.vertices = len(self.network)
 
     ''' Reads input file and stores as adjacency matrix with accompanying dictionary for node index '''
     def parse_file(self):
@@ -36,7 +33,49 @@ class flow_networks:
             for line in csvFile:
                 self.network[self.nodes_index[line[0]]][self.nodes_index[line[1]]] = int(line[2])
 
+    def BFS(self, s, t, parent):
+        visited = [False]*(self.vertices)
 
+        queue = []
+
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            u = queue.pop(0)
+
+            for ind, val in enumerate(self.network[u]):
+                if visited[ind] == False and val > 0:
+                    queue.append(ind)
+                    visited[ind] = True
+                    parent[ind] = u
+                    if ind == t:
+                        return True
+
+        return False
+
+    def FordFulkerson(self, source, sink):
+        parent = [-1]*(self.vertices)
+
+        max_flow = 0
+
+        while self.BFS(source, sink, parent):
+            path_flow = float("Inf")
+            s = sink
+            while s != source:
+                path_flow = min(path_flow, self.network[parent[s]][s])
+                s = parent[s]
+
+            max_flow += path_flow
+            v = sink
+
+            while v != source:
+                u = parent[v]
+                self.network[u][v] -= path_flow
+                self.network[v][u] += path_flow
+                v = parent[v]
+
+        return max_flow
 
 # printing out input file
 print(f'Input File: {sys.argv[1]}\n')
@@ -44,14 +83,14 @@ print("Input:")
 
 n = flow_networks(sys.argv[1])
 
-print(n.ford_fulkerson())
+source = n.nodes_index['s']
+sink = n.nodes_index['t']
 
-# print(len(n.network))
+max_flow = n.FordFulkerson(source,sink)
 
-# TODO: Edmonds-Karp here
 
 # printing out network flow
 # print("\nNetwork Flow:\n")
-# for edge in network:
+# for edge in n.network:
 #     print(f'{edge[0]},{edge[1]}\tflow: {edge[3]}/{edge[2]}')
-# print("\nMax flow in this network is",max_flow)
+print("\nMax flow in this network is",max_flow)
